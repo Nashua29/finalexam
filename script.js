3026b0a1bde2d8518037b225b1fa16864d35b12b
Nashua29 <nasuaalhubu@gmail.com>
2024-04-26 09:17:49 +0600

    3rd Push

---
+++ script.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const searchInput = document.getElementById('searchInput');
+  const searchButton = document.getElementById('searchButton');
+  const mealResults = document.getElementById('mealResults');
+  const showAllButton = document.getElementById('showAllButton');
+
+  searchButton.addEventListener('click', function () {
+      const searchTerm = searchInput.value.trim();
+      if (searchTerm !== '') {
+          searchMeal(searchTerm);
+      }
+  });
+
+  showAllButton.addEventListener('click', function () {
+      showAllMeals();
+  });
+
+  function searchMeal(term) {
+      clearResults();
+      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+          .then(response => response.json())
+          .then(data => {
+              const meals = data.meals;
+              if (meals) {
+                  meals.slice(0, 5).forEach(meal => {
+                      displayMeal(meal);
+                  });
+                  if (meals.length > 5) {
+                      showAllButton.style.display = 'block';
+                  } else {
+                      showAllButton.style.display = 'none';
+                  }
+              } else {
+                  mealResults.innerHTML = '<p>No meals found. Please try another search term.</p>';
+              }
+          })
+          .catch(error => {
+              console.error('Error fetching data:', error);
+          });
+  }
+
+  function showAllMeals() {
+      const searchTerm = searchInput.value.trim();
+      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+          .then(response => response.json())
+          .then(data => {
+              const meals = data.meals;
+              clearResults();
+              meals.forEach(meal => {
+                  displayMeal(meal);
+              });
+              showAllButton.style.display = 'none';
+          })
+          .catch(error => {
+              console.error('Error fetching data:', error);
+          });
+  }
+
+  function displayMeal(meal) {
+      const mealDiv = document.createElement('div');
+      mealDiv.classList.add('meal');
+
+      mealDiv.innerHTML = `
+          <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+          <h2>${meal.strMeal}</h2>
+          <p>${meal.strCategory}</p>
+          <p>${meal.strInstructions}</p>
+      `;
+
+      mealResults.appendChild(mealDiv);
+  }
+
+  function clearResults() {
+      mealResults.innerHTML = '';
+  }
+});
